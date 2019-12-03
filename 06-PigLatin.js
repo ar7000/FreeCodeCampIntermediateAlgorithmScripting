23934ff41f38593274ba9aa1f530c3589364d1ef
Ash Robinson <a_p_robinson@icloud.com>
2019-12-03 00:00:28 +0000

    Updated Pig Latin with alternative solution

--- 06-PigLatin.js
+++ 06-PigLatin.js
@@ -23,3 +23,22 @@ function translatePigLatin(str) {
   }
   
   translatePigLatin("rhythm");
+
+  //Can also be done with regular expressions...
+
+  function translatePigLatin(str) {
+    let startVowel = /^[aeiou]+/;
+    let consAndVowels = /\b[b-df-hj-np-tv-z]+(?=[aeiou])/;
+    let noVowels = /[b-df-hj-np-tv-z]+/;
+  
+    if(str.match(startVowel)){
+      return str + "way";
+    } else if(str.match(consAndVowels)){
+      return str.replace(str.match(consAndVowels), "") + str.match(consAndVowels) + "ay";
+    } else if(str.match(noVowels)){
+      return str + "ay";
+    }
+  
+  }
+  
+  translatePigLatin("rhythm");
